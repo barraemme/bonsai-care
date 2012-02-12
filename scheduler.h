6c612c75c2adab79bba7f021c0e8db54f1d5f79e
unknown <Mattia@Mattia-PC.(none)>
2012-02-12 21:03:51 +0100

    added scheduler

---
+++ scheduler.h
@@ -0,0 +1,22 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <QSqlDatabase>
+#include "bonsai.h"
+#include "speciemodel.h"
+
+class Scheduler: public QObject
+{
+
+    Q_OBJECT
+public:
+    Scheduler();
+    virtual ~Scheduler();
+
+private:
+    SpecieModel* m_itemmodel;
+    QSqlDatabase db;
+
+};
+
+#endif // SCHEDULER_H
